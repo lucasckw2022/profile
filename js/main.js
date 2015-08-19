ef88786d5673904128513390cd94e6eb6337f924
yeeie201 <lucas20229763@hotmail.com>
2015-08-19 08:49:09 +0800

    confirm

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-			$("div.block").mouseenter(function(){
+			$("div.block.list").mouseenter(function(){
 				$(this).find(".projectName").slideUp();
 				$(this).find(".projectDescription").delay(300).fadeIn(500);
 				$(this).find(".projectIcon img").delay(300).fadeIn(500);
 				});
-			$("div.block").mouseleave(function(){
+			$("div.block.list").mouseleave(function(){
 				$(this).find(".projectName").delay(400).slideDown();
 				$(this).find(".projectDescription").fadeOut(400);
 				$(this).find(".projectIcon img").fadeOut(500);
