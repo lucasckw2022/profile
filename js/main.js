c5a4d5d3b62c176d20dad21d0201c4d85ec2725a
yeeie201 <lucas20229763@hotmail.com>
2015-10-25 23:41:38 +0800

    confirm

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 			$("div.block.list").mouseenter(function(){
 				$(this).find(".projectName").slideUp();
 				$(this).find(".projectDescription").delay(300).fadeIn(500);
@@ -9,8 +9,44 @@ $(document).ready(function(){
 				$(this).find(".projectDescription").fadeOut(400);
 				$(this).find(".projectIcon img").fadeOut(500);
 				});
-			$("ul.mobile.menu").click(function(){
+			
+		});	*/
+			$(document).scroll(scrollcount)	
+			$(document).ready(popUp)
+			$(document).ready(mobileMenu)
+			
+			function scrollcount(){
+				i = 2;
+				backgroundImg = "url(image/background/0" + i +".jpg) no-repeat fixed"
+				checkposition = $(".contentBox > div:nth-child("+ i +")").position().top;
+				scrollPosition = $(document).scrollTop();
+				if (scrollPosition >= checkposition){
+					$("body").css({"background": backgroundImg}).fadeIn(1000);
+					i++
+				}
+				else if (scrollPosition < checkposition){
+					i--;
+					backgroundImg = "url(image/background/0" + i +".jpg) no-repeat fixed"
+					$("body").css({"background": backgroundImg}).fadeIn(1000);
+				}
+			}
+			
+
+			function popUp(){
+				$(".block").click(function(){
+					findFirstClass = "." + $(this).attr("class").split(" ")[0];
+					findSameClass = $(".projectList").find(findFirstClass);
+					$(findSameClass).css({"visibility": "visible", "opacity": "1"});
+				});
+				$(".popUpFrame").click(function(){
+					$(".popUpFrame").css({"opacity": "0","visibility": "hidden"});
+				})
+			}
+
+			function mobileMenu(){
+				$("ul.mobile.menu").click(function(){
 				$(this).find("ul").slideToggle();
 				});
-			});
+			}
+			
 			
