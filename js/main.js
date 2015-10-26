d72d4924f676a0d22a9e34df11771974d8738bf0
yeeie201 <lucas20229763@hotmail.com>
2015-10-27 01:05:33 +0800

    confirm

--- js/main.js
+++ js/main.js
@@ -69,6 +69,7 @@
 					scrollPosition <= $(".background1").position().top){
 					$(".menu li").removeClass("active")
 					$(".menu li:first-child").addClass("active")
+					$(".mobile.menu").removeClass("active")
 				}
 				else if(scrollPosition >=$("#project").position().top){
 					$(".menu li").removeClass("active")
